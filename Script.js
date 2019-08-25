fa81dd06a627931d06d227257f3daffe99f6b7c9
Micho27 <36533936+Micho27@users.noreply.github.com>
2019-08-25 19:02:55 +0100

    initial files
    Most pages only have temporary content. Mainly just fram of the site

---
+++ Script.js
@@ -0,0 +1,21 @@
+function MenuAndHeader() {
+	var body=document.getElementById("body");
+	var main=document.getElementById("main");
+
+	//create header
+	var header=document.createElement("header");
+	header.innerHTML+="<h1>GK Recovery Products</h1>";
+
+	//create menu and fill it
+	var div=document.createElement("div");
+	div.setAttribute('id','menu');
+	div.innerHTML+="<a id='link' href='index.html'>Home</a>";
+	div.innerHTML+="<a id='link' href='products.html'>Products</a>";
+	div.innerHTML+="<a id='link' href='contact.html'>Contact</a>";
+	div.innerHTML+="<a id='link' href='about.html'>About</a>";
+	div.innerHTML+="<a id='link' href='news.html'>News</a>";
+
+	//insert menu and header
+	body.insertBefore(header,main);
+	body.insertBefore(div,main);
+}
