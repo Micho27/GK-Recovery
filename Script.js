f1b25398f6d49a5c47c12a0a76d45816722b5915
Micho27 <36533936+Micho27@users.noreply.github.com>
2019-08-27 00:10:02 +0100

    Update JavaScript
    Adding footer to each page and changing link from products to services

--- Script.js
+++ Script.js
@@ -9,12 +9,18 @@ function MenuAndHeader() {
 	var div=document.createElement("div");
 	div.setAttribute('id','menu');
 	div.innerHTML+="<a id='link' href='index.html'>Home</a>";
-	div.innerHTML+="<a id='link' href='products.html'>Services</a>";
+	div.innerHTML+="<a id='link' href='services.html'>Services</a>";
 	div.innerHTML+="<a id='link' href='contact.html'>Contact</a>";
 	div.innerHTML+="<a id='link' href='about.html'>About</a>";
 	div.innerHTML+="<a id='link' href='news.html'>News</a>";
 
-	//insert menu and header
+	var footer=document.createElement("footer");
+	footer.innerHTML+="<p> E-mail:<a href='mailto:gkrecoveryproducts@gmail.com'>gkrecoveryproducts@gmail.com</a></p>";
+	footer.innerHTML+="<p> Phone: 087 928-2114";
+	footer.innerHTML+="<a href='https://www.facebook.com/gkrecoveryproducts'>Facebook</a><p>or</p><a href='https://www.instagram.com/gkrecovpro/'>Instagram</a>";
+	
+	//insert menu/header/footer
 	body.insertBefore(header,main);
 	body.insertBefore(div,main);
+	body.append(footer);
 }
