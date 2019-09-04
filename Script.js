a1b4edf82cc47e6a1637d102b061cd48255ec9c4
Micho27 <36533936+Micho27@users.noreply.github.com>
2019-09-04 04:29:04 +0100

    Update Script
    Brand new design. Added burger to be more responsive on mobile. Also added slideshow to home page.

--- Script.js
+++ Script.js
@@ -1,20 +1,91 @@
+//creates nav element for links to other pages
 function MenuAndHeader() {
 	var body=document.getElementById("body");
 	var main=document.getElementById("main");
+	
+	//create element that stores navigation bar
+	var nav=document.createElement("nav");
 
-	//create header
-	var header=document.createElement("header");
+	//create div for business name
+	nav.innerHTML+="<div class='logo'><h4>GKRecoveryPro</h4></div>";
 
-	//create menu and fill it
-	var div=document.createElement("div");
-	div.setAttribute('id','menu');
-	div.innerHTML+="<a id='link' href='index.html'>Home</a>";
-	div.innerHTML+="<a id='link' href='services.html'>Services</a>";
-	div.innerHTML+="<a id='link' href='contact.html'>Contact</a>";
-	div.innerHTML+="<a id='link' href='about.html'>About</a>";
-	div.innerHTML+="<a id='link' href='news.html'>News</a>";
+	//create list that has links
+	var ul=document.createElement("ul");
+	ul.setAttribute('class','navLinks');
+	ul.innerHTML+="<li><a href='index.html'>Home</a></li>";
+	ul.innerHTML+="<li><a href='about.html'>About</a></li>";
+	ul.innerHTML+="<li><a href='services.html'>Services</a></li>";
+	ul.innerHTML+="<li><a href='products.html'>Products</a></li>";
+	ul.innerHTML+="<li><a href='contact.html'>Contact</a></li>";
+	ul.innerHTML+="<li><a href='news.html'>News</a></li>";
+	nav.append(ul);
 
-	//insert menu/header/footer
-	body.insertBefore(header,main);
-	body.insertBefore(div,main);
+	//create burger
+	var burger=document.createElement("div");
+	burger.setAttribute('class','burger');
+	burger.innerHTML+="<div class='line1'></div>";
+	burger.innerHTML+="<div class='line2'></div>";
+	burger.innerHTML+="<div class='line3'></div>";
+	nav.append(burger);
+
+	//add nav to body
+	body.prepend(nav,main);
+	navSlide();
+}
+
+//navigaton bar functionality
+const navSlide = () => {
+	const burger=document.querySelector('.burger');
+	const nav=document.querySelector('.navLinks');
+	const navLinks=document.querySelectorAll('.navLinks li');
+	
+	burger.addEventListener('click',() => {
+		//toggle nav
+		nav.classList.toggle('nav-active');
+
+		//animate links
+		navLinks.forEach((link, index) => {
+			if(link.style.animation){
+				link.style.animation='';
+			}
+			else{
+				link.style.animation='navLinksFade 1s ease forwards';
+			}
+			console.log(index/7);
+		})
+
+		//burger animation
+		burger.classList.toggle('toggle');
+	});
+}
+
+//initial slide
+var slideIndex=1;
+showSlides(slideIndex);
+
+//prev/next functionality
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+//dots working
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+//change slide
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";//slideIndex is undefined  
+  dots[slideIndex-1].className += " active";
 }
